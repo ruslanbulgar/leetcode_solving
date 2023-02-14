841fff7c515b99734a904c8faf25e1f1ca7c372f
ruslanbulgar <rbulgar@gmail.com>
2023-02-14 13:36:01 +0200

    Roman To Arabic conversion

---
+++ src/main/java/com/rbcode/leatcode/roman_to_arabic/Solution.java
@@ -0,0 +1,46 @@
+package com.rbcode.leatcode.roman_to_arabic;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    private static Map<Character, Integer> SYMBOL_MAPPING = new HashMap<>() {
+        {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }
+    };
+
+    public int romanToInt(String s) {
+        if(s.matches("^[IVXLCDM]{1,15}$")) {
+            return convert(s);
+        }
+        return 0;
+    }
+
+    private int convert(String romanNumber) {
+        List<Integer> numbers
+                = romanNumber.codePoints().mapToObj(c -> SYMBOL_MAPPING.get((char) c)).collect(Collectors.toList());
+
+        Integer response = 0;
+        for(int i = numbers.size()- 1; i>= 0; i--) {
+            if(i-1 >= 0 && numbers.get(i) > numbers.get(i-1)) {
+                response += numbers.get(i) - numbers.get(i-1);
+                i = i-1;
+            } else {
+                response+=numbers.get(i);
+            }
+        }
+        return response;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().romanToInt("MCMXCIV"));
+    }
+}
